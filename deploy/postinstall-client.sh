e2f6b8d3f48825198de8cb4a47ca590c29dd854b
Matin Habibi <m.habibi@wallex.net>
2026-05-15 19:57:49 +0330

    restart service on dpkg install/upgrade

--- deploy/postinstall-client.sh
+++ deploy/postinstall-client.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 systemctl daemon-reload
 systemctl enable mattube-client
+systemctl restart mattube-client
--- deploy/postinstall-server.sh
+++ deploy/postinstall-server.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 systemctl daemon-reload
 systemctl enable mattube-server
+systemctl restart mattube-server
